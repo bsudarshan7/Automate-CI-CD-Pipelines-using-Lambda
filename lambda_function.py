8f3591b7b2092ea254365f6cd084ecf81d7afea1
bsudarshan7 <161239316+bsudarshan7@users.noreply.github.com>
2026-06-29 19:51:47 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,13 @@
 import json
+from datetime import datetime
 
 def lambda_handler(event, context):
-    # TODO implement
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from version2')
+        "statusCode": 200,
+        "body": json.dumps({
+            "application": "Lambda CI/CD Demo",
+            "version": "1.0",
+            "deployed_at": str(datetime.utcnow()),
+            "status": "Running Successfully"
+        })
     }
